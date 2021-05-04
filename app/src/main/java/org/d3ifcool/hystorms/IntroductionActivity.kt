3e7de935ffe351c29ff9c2c46e684d3c7620bda2
Andiez Satria <andis.permana12@gmail.com>
2021-05-04 15:48:20 +0700

    Create UI's for Authentication process

---
+++ app/src/main/java/org/d3ifcool/hystorms/IntroductionActivity.kt
@@ -0,0 +1,87 @@
+package org.d3ifcool.hystorms
+
+import android.Manifest
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.github.appintro.AppIntro2
+import com.github.appintro.AppIntroFragment
+import com.github.appintro.AppIntroPageTransformerType
+
+class IntroductionActivity : AppIntro2() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTransformer(AppIntroPageTransformerType.Fade)
+        setProgressIndicator()
+        isWizardMode = true
+        addSlide(
+            AppIntroFragment.newInstance(
+                "Selamat Datang!",
+                "Selamat datang di aplikasi Hystorms untuk pertama kalinya",
+                imageDrawable = R.drawable.text_hystorms,
+                backgroundDrawable = R.drawable.bg_slide,
+                descriptionColor = getColor(R.color.black),
+                titleColor = getColor(R.color.green_text),
+            )
+        )
+        addSlide(
+            AppIntroFragment.newInstance(
+                "Mulai Hal yang Baru",
+                "Mari mulai hijaukan sekitarmu dengan Hidroponik",
+                imageDrawable = R.drawable.ic_slide_3,
+                backgroundDrawable = R.drawable.bg_slide,
+                descriptionColor = getColor(R.color.black),
+                titleColor = getColor(R.color.green_text)
+            )
+        )
+        addSlide(
+            AppIntroFragment.newInstance(
+                "Hystorms",
+                "Hystorm adalah aplikasi monitoring tanaman Hidroponik. Hystorms membantu mengawasi dan mengelola tanaman Hidroponik dengan bantuan IoT Device",
+                imageDrawable = R.drawable.ic_slide_2,
+                backgroundDrawable = R.drawable.bg_slide,
+                descriptionColor = getColor(R.color.black),
+                titleColor = getColor(R.color.green_text)
+            )
+        )
+        addSlide(
+            AppIntroFragment.newInstance(
+                "Media dan Kamera",
+                "Hystorms memerlukan ijin untuk mengakses kamera dan media di dalam smartphone. Anda dapat memberi ijin pada lain waktu",
+                imageDrawable = R.drawable.ic_slide_4,
+                backgroundDrawable = R.drawable.bg_slide,
+                descriptionColor = getColor(R.color.black),
+                titleColor = getColor(R.color.green_text)
+            )
+        )
+        addSlide(
+            AppIntroFragment.newInstance(
+                "Lokasi Pengguna",
+                "Hystorms memerlukan ijin untuk mengetahui lokasimu.",
+                imageDrawable = R.drawable.ic_slide_5,
+                backgroundDrawable = R.drawable.bg_slide,
+                descriptionColor = getColor(R.color.black),
+                titleColor = getColor(R.color.green_text)
+            )
+        )
+        askForPermissions(
+            arrayOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ), 4, false
+        )
+        askForPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 5, true)
+    }
+
+    override fun onSkipPressed(currentFragment: Fragment?) {
+        super.onSkipPressed(currentFragment)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    override fun onDonePressed(currentFragment: Fragment?) {
+        super.onDonePressed(currentFragment)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
---
+++ app/src/main/java/org/d3ifcool/hystorms/ui/SplashFragment.kt
@@ -0,0 +1,19 @@
+package org.d3ifcool.hystorms.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.d3ifcool.hystorms.R
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+}
