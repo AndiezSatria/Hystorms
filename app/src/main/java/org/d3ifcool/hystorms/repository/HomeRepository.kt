df8a2619a402d851ded22ad1da8578e9cb6f23ca
Andiez Satria <andis.permana12@gmail.com>
2021-05-23 10:16:14 +0700

    Add function getWeather

--- app/src/main/java/org/d3ifcool/hystorms/repository/HomeRepository.kt
+++ app/src/main/java/org/d3ifcool/hystorms/repository/HomeRepository.kt
@@ -1,5 +1,6 @@
 package org.d3ifcool.hystorms.repository
 
+import androidx.lifecycle.MutableLiveData
 import org.d3ifcool.hystorms.BuildConfig
 import org.d3ifcool.hystorms.db.weather.WeatherCacheMapper
 import org.d3ifcool.hystorms.db.weather.WeatherDao
@@ -7,6 +8,7 @@ import org.d3ifcool.hystorms.model.DataOrException
 import org.d3ifcool.hystorms.model.Weather
 import org.d3ifcool.hystorms.network.WeatherNetworkMapper
 import org.d3ifcool.hystorms.network.WeatherRetrofit
+import org.d3ifcool.hystorms.util.ViewState
 
 class HomeRepository constructor(
     private val weatherService: WeatherRetrofit,
@@ -16,20 +18,33 @@ class HomeRepository constructor(
     // Firestore Tank Reference
     // Firestore Schedule Reference
 ) {
+    val weatherMutableLiveData: MutableLiveData<DataOrException<Weather, Exception>> =
+        MutableLiveData()
+
+    val weatherViewState: MutableLiveData<ViewState> = MutableLiveData(ViewState.NOTHING)
+
+    private fun setWeatherState(state: ViewState) = state.also { weatherViewState.value = it }
+
     suspend fun getWeather(
         lat: Long,
         long: Long,
         language: String,
     ) {
         val dataOrException: DataOrException<Weather, Exception> = DataOrException()
+        setWeatherState(ViewState.LOADING)
         try {
             val networkWeather = weatherService.get(lat, long, BuildConfig.APPLICATION_ID, language)
             val weather = networkMapper.mapFromEntity(networkWeather)
             weatherDao.insert(cacheMapper.mapFromDomain(weather))
-            val cacheWeather = weatherDao.getLatestCache()
+            val cacheWeather = weatherDao.getLatestCache().also {
+                setWeatherState(ViewState.SUCCESS)
+            }
             dataOrException.data = cacheMapper.mapFromEntity(cacheWeather)
+            weatherMutableLiveData.value = dataOrException
         } catch (e: Exception) {
             dataOrException.exception = e
+            weatherMutableLiveData.value = dataOrException
+            setWeatherState(ViewState.ERROR)
         }
     }
 }
