d902d185d3bc2067a5c66d30f7617beb8e195d71
Andiez Satria <andis.permana12@gmail.com>
2021-05-25 12:11:50 +0700

    Add firestore collection name for devices

--- app/src/main/java/org/d3ifcool/hystorms/constant/Constant.kt
+++ app/src/main/java/org/d3ifcool/hystorms/constant/Constant.kt
@@ -5,6 +5,7 @@ class Constant {
         const val PROFILE_PICTURE: String = "images/profile/"
         const val APP_DEBUG: String = "hystorms_debug"
         const val USERS: String = "users"
+        const val DEVICES: String = "devices"
         const val USER: String = "user_firebase"
         const val RC_PROFILE_IMAGE = 101
     }
